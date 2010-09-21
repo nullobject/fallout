92671b7a43f1f2343eb14d9b6b092a33d6781713
Josh Bassett <josh.bassett@gmail.com>
2010-09-22 07:01:22 +1000

    WIP

--- lib/fallout/marquee.rb
+++ lib/fallout/marquee.rb
@@ -5,21 +5,28 @@ module Fallout
   # Displays scrolling messages on a LED matrix display.
   # Runs in its own thread and has callbacks for various events.
   class Marquee
+    LEFT_TO_RIGHT =  1
+    RIGHT_TO_LEFT = -1
+
+    WIDTH  = 8
+    HEIGHT = 8
+
+    FONT_FACE = "monospace"
+    FONT_SIZE = 10.0
+
+    BLACK = [0.0, 0.0, 0.0].freeze
+    WHITE = [1.0, 1.0, 1.0].freeze
+
     attr_accessor :speed, :dir, :color
 
-    BLACK = [0.0, 0.0, 0.0, 1.0].freeze
-    WHITE = [1.0, 1.0, 1.0, 1.0].freeze
+    def initialize(spi, options = {})
+      @spi = spi
 
-    def initialize(options = {})
-      @speed  = options[:speed] || 1
-      @dir    = options[:dir]   || :right_to_left
-      @color  = options[:color] || WHITE
-      @x_pos  = 0
+      @delay = options[:delay] || 0.05
+      @dir   = options[:dir]   || RIGHT_TO_LEFT
+      @color = options[:color] || WHITE
 
-      @running = false
-
-      @surface = Cairo::ImageSurface.new(8, 8)
-      @context = Cairo::Context.new(@surface)
+      init_surface
     end
 
     def start
@@ -29,7 +36,7 @@ module Fallout
           tick
         end
       end
-      on_finished
+      when_finished
     end
 
     def stop
@@ -42,63 +49,77 @@ module Fallout
 
     def tick
       if @current_message
-        bytes = draw_text(@x_pos, 8)
-#         matrix_set_buffer(spi, bytes)
-        @x_pos -= 1
+        clear
+        draw_text(@x_pos, HEIGHT)
+        @spi.tx(surface_data)
 
-        sleep 0.05
+        @x_pos += @dir
 
-        # Has the message finished displaing?
-        stop_message if @x_pos < -50
+        sleep @delay
+
+        finish_message if finished?
       end
     end
 
     # Start displaying the given message.
     def start_message(text)
-      @current_message = text
-      @x_pos = 0
-      on_started
+      puts "DEBUG: start_message '#{text}'"
+      set_current_message(text)
+      when_started
     end
 
     # Stop displaying the current message.
-    def stop_message
+    def finish_message
+      puts "DEBUG: finish_message"
       @current_message = nil
-      @x_pos = 0
-      on_finished
+      when_finished
     end
 
-    def on_started(&block)
+    def when_started(&block)
       if block_given?
-        @on_started = block
+        @when_started = block
       else
-        @on_started.call(self) if @on_started
+        @when_started.call(self) if @when_started
       end
     end
 
-    def on_finished(&block)
+    def when_finished(&block)
       if block_given?
-        @on_finished = block
+        @when_finished = block
       else
-        @on_finished.call(self) if @on_finished
+        @when_finished.call(self) if @when_finished
       end
     end
 
   private
 
-    def draw_text(x_offset, y_offset)
-      @context.set_source_rgba(*BLACK)
-      @context.paint
+    def init_surface
+      @surface = Cairo::ImageSurface.new(WIDTH, HEIGHT)
+      @context = Cairo::Context.new(@surface)
 
-      @context.set_source_rgba(*WHITE)
-      @context.select_font_face("monospace", Cairo::FONT_SLANT_NORMAL, Cairo::FONT_WEIGHT_NORMAL)
-      @context.set_font_size 10.0
-      @context.move_to x_offset, y_offset
-      @context.text_path @current_message
-      @context.set_line_width 0.125
-      @context.set_antialias Cairo::ANTIALIAS_NONE
+      @context.select_font_face(FONT_FACE, Cairo::FONT_SLANT_NORMAL, Cairo::FONT_WEIGHT_NORMAL)
+      @context.set_font_size(FONT_SIZE)
+    end
+
+    def clear
+      @context.set_source_rgb(*BLACK)
+      @context.paint
+    end
+
+    def draw_text(x_offset, y_offset)
+      @context.set_source_rgb(*@color)
+      @context.move_to(x_offset, y_offset)
+
+#       @context.show_text(@current_message)
+
+      @context.text_path(@current_message)
+      @context.set_line_width(0.125)
+      @context.set_antialias(Cairo::ANTIALIAS_NONE)
       @context.fill_preserve
       @context.stroke
+    end
 
+    def surface_data
       [].tap do |bytes|
         e = @surface.data.enum_for(:each_byte)
 
@@ -110,6 +131,29 @@ module Fallout
           byte = (r << 5) | (g << 2) | b
           bytes << byte
         end
+      end
+    end
+
+    def set_current_message(text)
+      @text_extents = @context.text_extents(text)
+      set_initial_x_pos
+      @current_message = text
+    end
+
+    def set_initial_x_pos
+      if @dir > 0
+        @x_pos = -(@text_extents.width.to_i)
+      else
+        @x_pos = WIDTH
+      end
+    end
+
+    # Returns true if we've moved past the last character in the message.
+    def finished?
+      if @dir > 0
+        @x_pos > WIDTH
+      else
+        @x_pos < -(@text_extents.width.to_i)
       end
     end
   end
