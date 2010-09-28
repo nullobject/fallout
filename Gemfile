60c2386ac285afa5e0b6015548ffcbfcf2de4eb9
Josh Bassett <josh.bassett@gmail.com>
2010-09-29 08:17:52 +1000

    WIP

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source :rubygems
 
-gem 'bucaneer', '~> 0.1.1'
-gem 'cairo',    '~> 1.10.0'
+gem 'bucaneer',   '~> 0.1.1'
+gem 'cairo',      '~> 1.10.0'
+gem 'simple-rss', '~> 1.2.3'
 
 group :development do
   gem 'rake',  '~> 0.8.7'
--- lib/fallout/source/hudson.rb
+++ lib/fallout/source/hudson.rb
@@ -1,4 +1,15 @@
+require 'simple-rss'
+
 module Fallout::Source
-  class Hudson
+  class Hudson < Base
+    def update
+      atom = SimpleRSS.parse(open("/Users/josh/tmp/all.atom"))
+      atom.items.each do |entry|
+        title = entry.title
+        project, build, status = title.scan(/(.+) #(\d+) \((.+)\)/).first
+        # TODO: check if build has been displayed before.
+        Fallout::Manager.instance.notify(project.upcase)
+      end
+    end
   end
 end
